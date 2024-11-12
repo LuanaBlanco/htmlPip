4ceee56697edf574bef0e053ce7d162cd188de7c
Luana Blanco <blancoluana@gmail.com>
2024-11-12 12:04:56 -0300

    subiendo html, css  js

---
+++ pip.js
@@ -0,0 +1,17 @@
+alert("pip");
+const apiUrl = "https://jsonplaceholder.typicode.com"
+
+const xhr = new XMLHttpRequest();
+function onRequestHandler(){
+   if(this.readyState == 4 && this.status == 200){
+
+       const data = JSON.parse(this.response);
+        const HTMLResponse = document.querySelector("#app");
+        const tpl = data.map(user =>`<li> ${user.name} </li>`);
+       HTMLResponse.innerHTML = `<ul> ${tpl} </ul>`;
+       console.log(data);
+    }
+}
+xhr.addEventListener("load", onRequestHandler);
+xhr.open("GET",`${apiUrl}/users`)
+xhr.send();
